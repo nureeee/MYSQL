0cd66465fba59953d3f81dda4dd4caa4e810613f
nuree <83565990+nureeee@users.noreply.github.com>
2021-05-13 18:25:47 +0900

    210513

---
+++ 210513_과제_산.py
@@ -0,0 +1,32 @@
+import pymysql as my 
+con = my.connect(host='127.0.0.1', user='root', password='asdf2996!', db='mydb')
+c = con.cursor()
+
+# c.execute('create table lunch(menu char(20), price int)')
+# c.execute('insert into lunch values("짜장면", 5000)')
+# c.execute('insert into lunch values("비빔밥", 8000)')
+# c.execute('insert into lunch values("돈까스", 7000)')
+
+c.execute('select * from mt')
+
+res = c.fetchall()
+
+print(res)
+print(res[0])
+print(res[0][0], res[0][1])
+print(res[1][0], res[1][1])
+
+print('************')
+print('산이름  해발 ')
+
+for i in res:
+    print(i[0], i[1])    
+print('************')
+
+#c.execute('create table m2(m char(20), a int')
+
+con.commit()
+c.close()
+
+con.close()
+
---
+++ 210513_과제_영화.py
@@ -0,0 +1,25 @@
+import pymysql as my 
+
+con = my.connect(host='127.0.0.1', user='root', password='asdf2996!', db='mydb')
+c = con.cursor()
+
+c.execute('insert into movie values("명량", "최민식", 1761)')
+c.execute('update movie set actor where actor = "김혜수"')
+c.execute('delete from movie where name = "광해"')
+
+c.execute('select * from movie')
+
+res = c.fetchall()
+
+print(res[0])
+print(res[0][0], res[0][1], res[0][2])
+print(res[1][0], res[1][1], res[1][2])
+print(res[2][0], res[2][1], res[2][2])
+
+
+
+con.commit()
+c.close()
+
+con.close()
+
